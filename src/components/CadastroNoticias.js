f84ffb211d50ed16fc4c5ae3d860966741602ab7
Bruno RC <brunorcx@hotmail.com>
2021-05-22 17:25:06 -0400

    Tela de cadastro de notícias

--- src/components/CadastroNoticias.js
+++ src/components/CadastroNoticias.js
@@ -1,7 +1,20 @@
+import "../styles/cadastroNoticias.css";
+
 const CadastroNoticias = () => {
   return (
-    <div>
-      <h1>Noticiais</h1>
+    <div className="main-noticias">
+      <div className="container" id="container">
+        <div className="form-container noticias-sign-in-container">
+          <form action="/">
+            <h1>Criar Notícia</h1>
+            <span>Por favor, informe os dados para cadastro</span>
+            <input type="text" placeholder="Título" />
+            <input type="date" placeholder="Data" />
+            <textarea name="Text1" rows="13"></textarea>
+            <button className="cadastrarNoticia">Cadastrar</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
